a4bb0a758f5b53ed08edadbc925966e130882967
eeshaan18 <77087292+eeshaan18@users.noreply.github.com>
2026-05-21 17:24:40 +0530

    Update CORS middleware to specify allowed origins

--- src/server.js
+++ src/server.js
@@ -8,7 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://inventory-management-fe-sigma.vercel.app'],
+    credentials: true
+}));
 app.use(express.json()); // Allows us to parse JSON payloads from Next.js
 
 // Initialize MongoDB
